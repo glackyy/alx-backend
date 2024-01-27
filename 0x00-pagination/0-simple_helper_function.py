a7d3cc36968a1bcecbd2e0569f1cee09cfa3847f
glackyy <othmanesticky@gmail.com>
2024-01-27 10:31:44 +0100

    0-simple_helper_function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -1,1 +1,12 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Function Containing definition of index_range"""
+from typing import Tuple
+
+
+def index_range(page: int, page_size: int) -> Tuple[int, int]:
+    """Returning a tuple of size two containing start and end index"""
+    start, end = 0, 0
+    for i in range(page):
+        start = end
+        end += page_size
+    return (start, end) 
