cb4c6b1d136febefb58603acfe2005da6c38e928
glackyy <othmanesticky@gmail.com>
2024-01-27 10:54:53 +0100

    1-simple_pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -1,1 +1,53 @@
-#!/usr/bin/env python3
+#!/usr/bin/python3
+"""Class with Methods to create simple pagination from csv data"""
+import csv
+from typing import List
+index_range = __import__('0-simple_helper_function').index_range
+
+
+class Server:
+    """Server class to paginate a database of popular baby names.
+    """
+    DATA_FILE = "Popular_Baby_Names.csv"
+
+    def __init__(self):
+        self.__dataset = None
+
+    def dataset(self) -> List[List]:
+        """
+        Reads from csv file and returns the dataset.
+        Returns:
+            List[List]: The dataset.
+        """
+        if self.__dataset is None:
+            with open(self.DATA_FILE) as f:
+                reader = csv.reader(f)
+                dataset = [row for row in reader]
+            self.__dataset = dataset[1:]
+
+        return self.__dataset
+
+    @staticmethod
+    def assert_positive_int_type(val: int) -> None:
+        """Asserting that the value is a positive int"""
+        assert type(val) is int and val > 0
+
+    def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        """Returning a page of the dataset"""
+        self.assert_positive_int_type(page)
+        self.assert_positive_int_type(page_size)
+        dataset = self.dataset()
+        start, end = index_range(page, page_size)
+        try:
+            data = dataset[start:end]
+        except IndexError:
+            data = []
+        return data
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> dict:
+        """Returning a page of the dataset"""
+        t_pages = len(self.dataset()) // page_size + 1
+        data = self.get_page(page, page_size)
+        info = {
+            "page"
+        }
