eabae75b56d98f53f4aea723a99ebe82a255428a
glackyy <othmanesticky@gmail.com>
2024-01-27 10:32:57 +0100

    0-simple_helper_function

--- 0x00-pagination/0-simple_helper_function.py
+++ 0x00-pagination/0-simple_helper_function.py
@@ -9,4 +9,4 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
     for i in range(page):
         start = end
         end += page_size
-    return (start, end) 
+    return (start, end)
