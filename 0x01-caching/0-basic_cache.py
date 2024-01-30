c13ce805d19727fd9c85b03129b45df5e60dfa0c
glackyy <othmanesticky@gmail.com>
2024-01-30 17:45:12 +0100

    basic_cache

--- 0x01-caching/0-basic_cache.py
+++ 0x01-caching/0-basic_cache.py
@@ -1,7 +1,8 @@
 #!/usr/bin/env python3
 """ BaseCaching module
 """
-from  base_caching import BaseCaching
+from base_caching import BaseCaching
+
 
 class BaseCaching(BaseCaching):
     """ BaseCaching defines: a class for caching information
