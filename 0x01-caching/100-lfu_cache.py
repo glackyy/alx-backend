a13f34f05f72d98b1344807f238b5b8a03a1a1d7
glackyy <othmanesticky@gmail.com>
2024-01-31 14:57:00 +0100

    LFUCache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -10,3 +10,25 @@ class LFUCache(BaseCaching):
         super().__init__()
         self.usage = []
         self.frequency = {}
+
+    def put(self, key, item):
+        """Caching a key-value pair"""
+        if key is None or item is None:
+            pass
+        else:
+            leng = len(self.cache_data)
+            if len >= BaseCaching.MAX_ITEMS and key not in self.cache_data:
+                lfu = min(self.frequency.values())
+                lfu_k = []
+                for k, val in self.frequency.items():
+                    if val == lfu:
+                        lfu_k.append(k)
+                if len(lfu_k) > 1:
+                    lru_lfu = {}
+                    for k in lfu_k:
+                        lru_lfu[k] = self.usage.index(k)
+                    disc = min(lru_lfu.values())
+                    disc = self.usage[disc]
+                else:
+                    disc = lfu_k[0]
+                
