78924a6ee29a3b09d9e51903f6b9976cec199962
glackyy <othmanesticky@gmail.com>
2024-01-31 14:27:12 +0100

    LFUCache

--- 0x01-caching/100-lfu_cache.py
+++ 0x01-caching/100-lfu_cache.py
@@ -5,4 +5,8 @@ from base_caching import BaseCaching
 
 class LFUCache(BaseCaching):
     """LFU CACHE defining a LFU caching sys"""
-    
+    def __init__(self):
+        """Initializing the class with the parent's init method"""
+        super().__init__()
+        self.usage = []
+        self.frequency = {}
