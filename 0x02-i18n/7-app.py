58961749b46a6bead9624a5f0df8d3002b5229ac
glackyy <othmanesticky@gmail.com>
2024-02-06 10:45:44 +0100

    7-app

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -23,3 +23,22 @@ class Config(object):
 app = Flask(__name__)
 app.config.from_object(Config)
 babel = Babel(app)
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
+
+
+def get_user() -> Union[Dict, None]:
+    """Returning a user Dict or none if ID can't be found
+    or login_as was not found"""
+    id = request.args.get('login_as', None)
+    if id and int(id) in users.keys():
+        return users.get(int(id))
+    return None
+
+
