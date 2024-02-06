6a9d11f7647868941241fc63f80223997defca86
glackyy <othmanesticky@gmail.com>
2024-02-06 10:30:20 +0100

    6-app

--- 0x02-i18n/6-app.py
+++ 0x02-i18n/6-app.py
@@ -1,1 +1,16 @@
 #!/usr/bin/env python3
+"""Flask App"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g
+)
+from flask_babel import Babel
+
+
+class Config(object):
+    """Configuring Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
