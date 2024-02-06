c2c2db2e16dbf8489e178ce0b159428a81bc387d
glackyy <othmanesticky@gmail.com>
2024-02-06 10:42:46 +0100

    7-app

--- 0x02-i18n/7-app.py
+++ 0x02-i18n/7-app.py
@@ -1,1 +1,14 @@
 #!/usr/bin/env python3
+"""Flask App"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g
+)
+from flask_babel import Babel
+from typing import (
+    Union,
+    Dict
+)
+
