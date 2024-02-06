a385e8f3f5b6e7d8ac29444738a61bdd7d662d5a
glackyy <othmanesticky@gmail.com>
2024-02-06 09:10:52 +0100

    2-app

--- 0x02-i18n/2-app.py
+++ 0x02-i18n/2-app.py
@@ -1,1 +1,23 @@
 #!/usr/bin/env python3
+"""Flask App"""
+from flask import (
+    Flask,
+    render_template,
+    request
+)
+from flask_babel import Babel
+
+
+class Config(object):
+    """Configuring Babel"""
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
