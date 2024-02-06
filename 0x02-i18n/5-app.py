42677ec23ee9c46f56c26b08785adc048fcc801c
glackyy <othmanesticky@gmail.com>
2024-02-06 10:17:58 +0100

    5-app

--- 0x02-i18n/5-app.py
+++ 0x02-i18n/5-app.py
@@ -1,1 +1,17 @@
 #!/usr/bin/env python3
+"""Flask App"""
+from flask import (
+    Flask,
+    render_template,
+    request,
+    g
+)
+from flask_label import Babel
+
+
+users = {
+    1: {"name": "Balou", "locale": "fr", "timezone": "Europe/Paris"},
+    2: {"name": "Beyonce", "locale": "en", "timezone": "US/Central"},
+    3: {"name": "Spock", "locale": "kg", "timezone": "Vulcan"},
+    4: {"name": "Teletubby", "locale": None, "timezone": "Europe/London"},
+}
