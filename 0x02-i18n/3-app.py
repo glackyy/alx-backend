f33551bbca5477444eaf2e7a2d20cd1784a7a799
glackyy <othmanesticky@gmail.com>
2024-02-06 09:25:51 +0100

    3-app

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -26,3 +26,15 @@ def get_locale():
     Select and return best language match based on supported languages
     """
     return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+
+@app.route('/', strict_slashes=False)
+def index() -> str:
+    """
+    Handles / route
+    """
+    return render_template('3-index.html')
+
+
+if __name__ == "__main__":
+    app.run(port="5000", host="0.0.0.0", debug=True)
