54f449cc13deaea9121506ea947aeddd05292bc6
glackyy <othmanesticky@gmail.com>
2024-03-05 11:55:19 +0100

    8-job_test

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -2,3 +2,17 @@ import { createQueue } from 'kue';
 import { describe, it, before, after, afterEach } from 'mocha';
 import { expect } from 'chai';
 import createPushNotificationJobs from './8-job.js';
+
+const queue = createQueue();
+
+describe('Test createNotificationJobs function', function() {
+  before(function () {
+    queue.testMode.enter();
+  });
+  afterEach(function () {
+    queue.testMode.clear();
+  });
+  after(function () {
+    queue.testMode.exit();
+  });
+})
