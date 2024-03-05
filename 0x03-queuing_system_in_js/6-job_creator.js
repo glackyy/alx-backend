b5959c635a0e8e923f81c0988be2b8d3db336c38
glackyy <othmanesticky@gmail.com>
2024-03-05 11:17:21 +0100

    6-job_creator

--- 0x03-queuing_system_in_js/6-job_creator.js
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -12,3 +12,9 @@ const job = queue.create('push_notification_code', notif).save(function (error) 
     console.log(`Notification job created: ${job.id}`);
   }
 });
+
+job.on('complete', function() {
+  console.log('Notification job completed');
+}).on('failed', function() {
+  console.log('Notification job failed');
+});
