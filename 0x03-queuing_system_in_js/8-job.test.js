47a367317ddcfdce65060d438ad48de03d25fa41
glackyy <othmanesticky@gmail.com>
2024-03-05 11:57:48 +0100

    8-job_test

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -15,4 +15,23 @@ describe('Test createNotificationJobs function', function() {
   after(function () {
     queue.testMode.exit();
   });
+
+  it('display an error message if jobs is not an array', function() {
+    expect(() => createPushNotificationJobs('job', queue)).to.throw(Error, 'Jobs is not an array');
+  });
+  
+  it('Test whether jobs are created', function() {
+    const jobs = [
+        {
+          phoneNumber: '4153518780',
+          message: 'This is the code 1234 to verify your account'
+        },
+        {
+          phoneNumber: '4153518781',
+          message: 'This is the code 4562 to verify your account'
+        },
+      ];
+
+      createPushNotificationJobs(jobs, queue);
+  })
 })
