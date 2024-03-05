827a882ab52bad037f57693a1d95e1270392ccf6
glackyy <othmanesticky@gmail.com>
2024-03-05 12:23:07 +0100

    9-stock

--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -5,3 +5,11 @@ import express from 'express';
 const app = express();
 const rdClient = createClient();
 
+rdClient.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
+
+rdClient.on('error', function(error) {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
