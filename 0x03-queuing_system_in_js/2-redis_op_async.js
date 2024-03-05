533f8165c10b68681cf79e3a6647a5919d548762
glackyy <othmanesticky@gmail.com>
2024-03-05 10:31:53 +0100

    1-redis_op

--- 0x03-queuing_system_in_js/2-redis_op_async.js
+++ 0x03-queuing_system_in_js/2-redis_op_async.js
@@ -1,1 +1,34 @@
-#!/usr/bin/node
+import { error } from 'console';
+import { Socket } from 'dgram';
+import { createClient, print } from 'redis';
+import { promisify } from 'util';
+
+const client = createClient();
+
+function redisConnect() {
+  client.on('connect', function() {
+    console.log('Redis client connected to the server');
+  }).on('error', (error) => {
+    console.log('Redis client not connected to the server: ${error}');
+  });
+};
+
+function setNewSchool(schoolName, value) {
+  client.set(schoolName, value, print);
+};
+
+const get = promisify(client.get).bind(client);
+
+async function displaySchoolValue(schoolName) {
+  const res = await get(schoolName).catch((error) => {
+    if (error) {
+        console.log(error);
+        throw error;
+    }
+  });
+  console.log(res);
+}
+displaySchoolValue('Holberton');
+setNewSchool('HolbertonSanFrancisco', '100');
+displaySchoolValue('HolbertonSanFrancisco');
+redisConnect();
