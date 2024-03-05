4917e315265a05cd0e82914fc6b1aa854edb433a
glackyy <othmanesticky@gmail.com>
2024-03-05 11:15:57 +0100

    6-job_creator

---
+++ 0x03-queuing_system_in_js/6-job_creator.js
@@ -0,0 +1,14 @@
+import { createQueue } from 'kue';
+
+const queue = createQueue();
+
+const notif = {
+  'phoneNumber': '0123456789',
+  'message': 'Code to verify your account'
+}
+
+const job = queue.create('push_notification_code', notif).save(function (error) {
+  if (!error) {
+    console.log(`Notification job created: ${job.id}`);
+  }
+});
