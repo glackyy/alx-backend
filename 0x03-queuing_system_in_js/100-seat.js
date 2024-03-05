12fd5d6a121792717b1089d0ce3460fb6d690ee1
glackyy <othmanesticky@gmail.com>
2024-03-05 12:53:07 +0100

    100-seat

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -6,11 +6,16 @@ import express from 'express';
 const app = express();
 const rdClient = createClient();
 
-redisClient.on('connect', function() {
-    console.log('Redis client connected to the server');
-  });
+rdClient.on('connect', function() {
+  console.log('Redis client connected to the server');
+});
   
-  redisClient.on('error', function (error) {
-    console.log(`Redis client not connected to the server: ${error}`);
-  });
-  
+rdClient.on('error', function (error) {
+  console.log(`Redis client not connected to the server: ${error}`);
+});
+
+const get = promisify(rdClient.get).bind(rdClient);
+
+function reserveSeat(number) {
+  rdClient.set('available_seats', number);
+}
