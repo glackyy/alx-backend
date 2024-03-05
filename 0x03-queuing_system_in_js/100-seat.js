0b112253d63209ab595e8f20cae04a49248e4415
glackyy <othmanesticky@gmail.com>
2024-03-05 12:57:45 +0100

    100-seat

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -5,6 +5,7 @@ import express from 'express';
 
 const app = express();
 const rdClient = createClient();
+const queue = createQueue();
 
 rdClient.on('connect', function() {
   console.log('Redis client connected to the server');
@@ -19,3 +20,28 @@ const get = promisify(rdClient.get).bind(rdClient);
 function reserveSeat(number) {
   rdClient.set('available_seats', number);
 }
+
+async function getCurrentAvailableSeats() {
+  const seats = await get('available_seats');
+  return seats;
+}
+
+let reservationEnabled = true;
+
+app.get('/available_seats', async function (req, res) {
+  const availableSeats = await getCurrentAvailableSeats();
+  res.json({"numberOfAvailableSeats": availableSeats});
+});
+
+app.get('/reserve_seat', function (req, res) {
+  if (!reservationEnabled) {
+    res.json({"status": "Reservation are blocked"});
+    return;
+  }
+  const job = queue.create('reserve_seat', {'seat': 1}).save((error) => {
+    if (error) {
+      res.json({"status": "Reservation failed"});
+      return;
+    }
+  })
+})
