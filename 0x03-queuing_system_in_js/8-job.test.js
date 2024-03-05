1591899dd8b6408272674c99fa50633bbffd476c
glackyy <othmanesticky@gmail.com>
2024-03-05 12:00:40 +0100

    8-job_test

--- 0x03-queuing_system_in_js/8-job.test.js
+++ 0x03-queuing_system_in_js/8-job.test.js
@@ -33,5 +33,11 @@ describe('Test createNotificationJobs function', function() {
       ];
 
       createPushNotificationJobs(jobs, queue);
-  })
-})
+
+      expect(queue.testMode.jobs.length).to.equal(2);
+      expect(queue.testMode.jobs[0].type).to.equal('push_notification_code_3');
+      expect(queue.testMode.jobs[0].data).to.eql(jobs[0]);
+      expect(queue.testMode.jobs[1].type).to.equal('push_notification_code_3');
+      expect(queue.testMode.jobs[1].data).to.eql(jobs[1]);
+  });
+});
