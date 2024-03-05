a8bf08aa5726aec8cb1165bac55519b8436274ec
glackyy <othmanesticky@gmail.com>
2024-03-05 12:25:57 +0100

    9-stock

--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -13,3 +13,11 @@ rdClient.on('error', function(error) {
   console.log(`Redis client not connected to the server: ${error}`);
 });
 
+const get = promisify(rdClient.get).bind(rdClient);
+
+const listProducts = [
+  {'itemId': 1, 'itemName': 'Suitcase 250', 'price': 50, 'initialAvailableQuantity': 4},
+  {'itemId': 2, 'itemName': 'Suitcase 450', 'price': 100, 'initialAvailableQuantity': 10},
+  {'itemId': 3, 'itemName': 'Suitcase 650', 'price': 350, 'initialAvailableQuantity': 2},
+  {'itemId': 4, 'itemName': 'Suitcase 1050', 'price': 550, 'initialAvailableQuantity': 5}
+];
