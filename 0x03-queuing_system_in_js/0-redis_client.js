51c74e6419d716f3d484cc606428ef44ffaf9a81
glackyy <othmanesticky@gmail.com>
2024-03-05 10:15:46 +0100

    0-redis-client

--- 0x03-queuing_system_in_js/0-redis_client.js
+++ 0x03-queuing_system_in_js/0-redis_client.js
@@ -2,4 +2,11 @@ import { createClient } from 'redis';
 
 function redisConnect() {
   const client = createClient();
-}
+  client.on('connect', function() {
+    console.log('Redis client connected to the server');
+  }).on('error', (error) => {
+    console.log('Redis client not connected to the server: ${error}');
+  });
+};
+
+redisConnect();
