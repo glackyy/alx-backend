9213fb82c8e62bbdeb6c5f0623eb5c4008119fc5
glackyy <othmanesticky@gmail.com>
2024-03-05 11:35:54 +0100

    7-job_processor

--- 0x03-queuing_system_in_js/7-job_processor.js
+++ 0x03-queuing_system_in_js/7-job_processor.js
@@ -10,4 +10,11 @@ function sendNotification(phoneNumber, message, job, done) {
     done(Error(`Phone number ${phoneNumber} is blacklisted`));
     return;
   }
+  job.progress(50, 100);
+  console.log(`Sending notification to ${phoneNumber}, with message: ${message}`);
+  done();
 }
+
+queue.process('push_notification_code_2', 2, function(job, done) {
+  sendNotification(job.data.phoneNumber, job.data.message, job, done);
+});
